be0703c423d6b36123925c8e8e52edb9342cafb2
Dmitry Vysochin <dmitry.vysochin@gmail.com>
2014-05-01 13:27:05 +0400

    To MOcaros

--- lib/exconfig.ex
+++ lib/exconfig.ex
@@ -1,34 +1,40 @@
+defmodule ExConfig do
+  defmacro __usinf__(_opts) do
 
-defmodule Config do
-  Dict.get(Mix.Project.config, :config_files, []) |> Enum.each fn({confkey, file}) ->
-    IO.puts "Create config :#{confkey} from file #{file}"
-    {:ok, toml} = :etoml.parse(File.read!(file))
-    toml |> Enum.each fn({key, value}) ->
-      def env(unquote(confkey), unquote(binary_to_atom(key))), do: unquote(value)  
-      def unquote(:"#{key}")() do
-        unquote(value)
-      end
-    end
-  end
+    quote do
+      defmodule Config do
 
 
-  def env(_, _), do: :error
+          Dict.get(Mix.Project.config, :config_files, []) |> Enum.map fn({confkey, file}) ->
+            IO.puts "Create config :#{confkey} from file #{file}"
+            {:ok, toml} = :etoml.parse(File.read!(file))
+            toml |> Enum.map fn({key, value}) ->
+              quote do 
+                def env(unquote(confkey), unquote(binary_to_atom(key))), do: unquote(value)  
+                def unquote(:"#{key}")(), do: unquote(value)
+              end
+            end
+          end
 
-  @doc """
-  Get value for key in default group
-  """
-  def env(key), do: env(:base, key)
+          def env(_, _), do: :error
 
-  @doc """
-  Get group data
-  """
-  def get(group, key), do: Dict.get(env(group), key)
-  def get(group, key, default), do: Dict.get(env(group), key, default)
+          @doc """
+          Get value for key in default group
+          """
+          def env(key), do: env(:base, key)
 
-  @doc """
-  Get all keys for default group
-  """
-  def keys(group), do: Dict.keys(env(group))
+          @doc """
+          Get group data
+          """
+          def get(group, key), do: Dict.get(env(group), key)
+          def get(group, key, default), do: Dict.get(env(group), key, default)
 
-end
+          @doc """
+          Get all keys for default group
+          """
+          def keys(group), do: Dict.keys(env(group))
 
+      end #end defmodule
+    end #end quote
+  end #end defmacro
+end # end defmodule ExConfig
